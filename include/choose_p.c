83fa6b230b0ce7f2982331fc96024ad27e60a2a3
Mathias Bazireau <mbazirea@k1r4p11.42mulhouse.fr>
2022-11-10 15:01:21 +0100

    fini

--- include/choose_p.c
+++ include/choose_p.c
@@ -6,48 +6,53 @@
 /*   By: mbazirea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 19:35:27 by mbazirea          #+#    #+#             */
-/*   Updated: 2022/11/10 14:04:46 by mbazirea         ###   ########.fr       */
+/*   Updated: 2022/11/10 15:01:00 by mbazirea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+int	choose_p2(char *s, int *c, int *a, int i)
+{
+	*c = write(1, &"0x", 2);
+	if (*c == -1)
+	{
+		free(s);
+		return (-1);
+	}
+	*a += *c;
+	while (i >= 0)
+	{
+		*c = write(1, &s[i], 1);
+		if (*c == -1)
+		{
+			free(s);
+			return (-1);
+		}
+		*a += *c;
+		i--;
+	}
+	return (0);
+}
+
 int	choose_p(va_list arg, int *a)
 {
 	unsigned long long	b;
-	char				*test;
+	char				*s;
 	int					c;
 	int					i;
 
 	b = va_arg(arg, unsigned long long);
-	test = ft_putnbr_base(b, "0123456789abcdef");
+	s = ft_putnbr_base(b, "0123456789abcdef");
 	i = 0;
-	if (test == NULL)
-	{
-		free(test);
+	if (s == NULL)
 		return (-1);
-	}
-	while (test[i])
+	while (s[i])
 		i++;
 	i--;
-	c = write(1, &"0x", 2);
+	c = choose_p2(s, &c, a, i);
 	if (c == -1)
-	{
-		free(test);
 		return (-1);
-	}
-	*a += c;
-	while (i >= 0)
-	{
-		c = write(1, &test[i], 1);
-		if (c == -1)
-		{
-			free(test);
-			return (-1);
-		}
-		*a += c;
-		i--;
-	}
-	free(test);
+	free(s);
 	return (0);
 }
